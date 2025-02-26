74b595c97d0ab509f4c6e3f7f2f4860a10c72652
Frank Geter <fgeter@gmail.com>
2025-02-26 16:13:50 -0700

    Minor spelling changes and changed to error stop on duplicate print objects in print.prt file.

--- src/actions.f90
+++ src/actions.f90
@@ -248,7 +248,7 @@
             if (pco%mgtout == "y") then
               ! write (2612, *) j, time%yrc, time%mo, time%day_mo, "        ", "IRRIGATE", phubase(j),  &
               write (2612, *) j, time%yrc, time%mo, time%day_mo, d_tbl%act(iac)%name, "IRRIGATE", phubase(j),  &
-                  pcom(j)%plcur(ipl)%phuacc, soil(j)%sw,pl_mass(j)%tot(ipl)%m, soil1(j)%rsd(1)%m, &
+                  pcom(j)%plcur(ipl)%phuacc, soil(j)%sw, pl_mass(j)%tot(ipl)%m, soil1(j)%rsd(1)%m, &
                   sol_sumno3(j), sol_sumsolp(j), irrig(j)%demand
             end if
             
