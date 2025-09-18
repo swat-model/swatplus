e3f7903dfda0cdc8f58262c98d7866a16a3216b6
fgeter <fgeter@gmail.com>
2025-09-18 08:13:42 -0600

    Minor change to basin_print_codes_read.f90 to prevent gfortran allocation error when reading print object labels. Modified mgt_harvresidue.f90 to use harvest efficiency from harv.ops input file if harvest efficiency in management schedule is zero and commented out code that was taking residue from meta, str, and lig pools and corrected code that was double reducing the carbon amount in the residue carbon pool.
    Modified mgt_sched.f90 to allow harvesting of residue when the crop is not living (i.e. after the crop has been killed) and modified the call to mgt_harvesresidue and modified the code to prevent killing crop that what was no longer growing.

--- src/mgt_harvresidue.f90
+++ src/mgt_harvresidue.f90
@@ -1,4 +1,4 @@
-      subroutine mgt_harvresidue (jj, harveff)
+      subroutine mgt_harvresidue (jj, harveff, iharvop)
 
 !!    ~ ~ ~ PURPOSE ~ ~ ~
 !!    this subroutine performs the harvest residue operation 
@@ -13,6 +13,7 @@
 
       use plant_module
       use carbon_module
+      use mgt_operations_module
       use organic_mineral_mass_module
       
       implicit none
@@ -20,20 +21,29 @@
       integer :: j = 0                  !none           |HRU number
       integer, intent (in) :: jj        !none           |hru number
       real, intent (in) :: harveff      !0-1            |harvest efficiency
+      real              :: eff          !0-1            |local scoope harvest efficiency
+      integer, intent (in) :: iharvop   !               |harvest operation type
       
       j = jj
+
+      if (harveff < .00001) then
+            eff = harvop_db(iharvop)%eff
+      else
+            eff = harveff
+      endif
       
       !! zero stover harvest
       hrc_d(j)%harv_stov_c = 0.
       
       !! harvest plant surface residue
-      soil1(j)%rsd(1) = (1. - harveff) * soil1(j)%rsd(1)
-      soil1(j)%meta(1) = (1. - harveff) * soil1(j)%meta(1)
-      soil1(j)%str(1) = (1. - harveff) * soil1(j)%str(1)
-      soil1(j)%lig(1) = (1. - harveff) * soil1(j)%lig(1)
+      soil1(j)%rsd(1) = (1. - eff) * soil1(j)%rsd(1)
+      ! soil1(j)%meta(1) = (1. - harveff) * soil1(j)%meta(1)
+      ! soil1(j)%str(1) = (1. - harveff) * soil1(j)%str(1)
+      ! soil1(j)%lig(1) = (1. - harveff) * soil1(j)%lig(1)
       
       !! compute carbon in harvested residue
-      hrc_d(j)%harv_stov_c = hrc_d(j)%harv_stov_c + 0.42 * (1. - harveff) * soil1(j)%rsd(1)%c
+      ! hrc_d(j)%harv_stov_c = hrc_d(j)%harv_stov_c + 0.42 * (1. - harveff) * soil1(j)%rsd(1)%c
+      hrc_d(j)%harv_stov_c = 0.42 * soil1(j)%rsd(1)%c
       
       return
       end  subroutine mgt_harvresidue
