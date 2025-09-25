abef801d1dc0dd4a4e491af7eddf181d8f08cd08
fgeter <fgeter@gmail.com>
2025-09-25 11:56:20 -0600

    Improve utils.f90 to write all error output to error_unit (i.e. stderr) Add [Tt]mp/ to .gitignore to ignore tmp folder

--- src/utils.f90
+++ src/utils.f90
@@ -4,24 +4,27 @@ module utils
 contains
 
 real function exp_w(y)
+      use iso_fortran_env
 #ifdef __INTEL_COMPILER
       use ifcore, only: tracebackqq
 #endif
     implicit none
     real, intent(in) :: y
     if (y < -80.) then
-        print *, "exp(", y, ") causes an underflow. Setting exp_w result to zero"
+        ! print *, "exp(", y, ") causes an underflow. Setting exp_w result to zero"
+        ! write(error_unit,'(A,F6.1,A)', advance='YES') "Warning: exp(", y, ") causes an underflow."
+        write(error_unit,'(A)') ""
+        write(error_unit,'(A,F6.1,A)') "Warning: exp(", y, ") causes an underflow."
+        write(error_unit,'(A)') "Setting exp_w result to zero"
         exp_w = 0.
-        print *
-        print *, "Stack Trace:" 
 #ifdef __INTEL_COMPILER
-        print *, "Using Intel Fortran compiler stack trace"
+        write(error_unit,'(A)') "Intel Fortran compiler stack trace"
         call tracebackqq(USER_EXIT_CODE=-1)
 #elif defined(__GFORTRAN__)
-        print *, "Using GNU Fortran compiler stack trace"
+        write(error_unit,'(A)') "Intel Fortran compiler stack trace"
         call backtrace()
 #else
-        print *, "No stack trace available: Unknown compiler"
+        write(error_unit, *)  "No stack trace available: Unknown compiler"
 #endif
     else  
         exp_w = exp(y)
