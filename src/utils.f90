e44da48101e701daac3c0e59f6213d5fe9ba45cc
fgeter <fgeter@gmail.com>
2025-09-25 12:33:46 -0600

    Added a err_output logical variable in utils.f90 to control whether output is generated if the exponent result causes and underflow.

--- src/utils.f90
+++ src/utils.f90
@@ -10,22 +10,27 @@ real function exp_w(y)
 #endif
     implicit none
     real, intent(in) :: y
+    logical :: err_output
+    
+    err_output = .true.
+    ! err_output = .false.
+
     if (y < -80.) then
-        ! print *, "exp(", y, ") causes an underflow. Setting exp_w result to zero"
-        ! write(error_unit,'(A,F6.1,A)', advance='YES') "Warning: exp(", y, ") causes an underflow."
-        write(error_unit,'(A)') ""
-        write(error_unit,'(A,F6.1,A)') "Warning: exp(", y, ") causes an underflow."
-        write(error_unit,'(A)') "Setting exp_w result to zero"
         exp_w = 0.
+        if (err_output) then
+            write(error_unit,'(A)') ""
+            write(error_unit,'(A,F6.1,A)') "Warning: exp(", y, ") causes an underflow."
+            write(error_unit,'(A)') "Setting exp_w result to zero"
 #ifdef __INTEL_COMPILER
-        write(error_unit,'(A)') "Intel Fortran compiler stack trace"
-        call tracebackqq(USER_EXIT_CODE=-1)
+            write(error_unit,'(A)') "Intel Fortran compiler stack trace"
+            call tracebackqq(USER_EXIT_CODE=-1)
 #elif defined(__GFORTRAN__)
-        write(error_unit,'(A)') "GNU Fortran compiler stack trace"
-        call backtrace()
+            write(error_unit,'(A)') "GNU Fortran compiler stack trace"
+            call backtrace()
 #else
-        write(error_unit, *)  "No stack trace available: Unknown compiler"
+            write(error_unit, *)  "No stack trace available: Unknown compiler"
 #endif
+        endif
     else  
         exp_w = exp(y)
     endif
