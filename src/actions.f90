5a9cb49a58ec78b3347e5d377df5d69a19a02cac
rtbailey <ryant.bailey@colostate.edu>
2025-10-21 13:53:16 -0600

    Added print statement to actions.f90

--- src/actions.f90
+++ src/actions.f90
@@ -87,6 +87,8 @@
       character(len=1) :: action = ""      !         |
       character(len=40) :: lu_prev = ""    !         |
 
+			print *, 'OK, I am here'
+			
       do iac = 1, d_tbl%acts
         action = "n"
         do ial = 1, d_tbl%alts
