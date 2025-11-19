c531fab8c8aea8b3bf47ac1246385e7c325f40dc
Taci Ugraskan <143652012+tugraskan@users.noreply.github.com>
2025-11-19 10:11:39 -0600

    1119 Jeffs Nam

--- src/wet_all_initial.f90
+++ src/wet_all_initial.f90
@@ -6,12 +6,10 @@
       implicit none
       
       integer :: iihru = 0      !              |hru counter
-      integer :: iprop = 0      !none          |pointer to surface storage data 
 
       do iihru = 1, sp_ob%hru
         !! set initial volumes and convert units
-        iprop = hru(iihru)%dbs%surf_stor
-        if (iprop > 0) then
+        if (hru(iihru)%dbs%surf_stor /= "null") then
           call wet_initial (iihru)
           wet_om_init(iihru) = wet(iihru)
         end if
