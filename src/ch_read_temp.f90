5e7073af6fc4a978d59d4e6db4ab496fbbe4d393
rtbailey <ryant.bailey@colostate.edu>
2026-02-12 15:02:58 -0700

    changes to reading initial channel temperature

--- src/ch_read_temp.f90
+++ src/ch_read_temp.f90
@@ -1,6 +1,7 @@
       subroutine ch_read_temp
       
       use basin_module
+      use time_module
       use input_file_module
       use maximum_data_module
       use channel_data_module
@@ -19,45 +20,48 @@
       
       inquire (file=in_cha%temp, exist=i_exist)
       if (.not. i_exist .or. in_cha%temp == "null") then
-        imax = 0    !allocate (w_temp(0:0))
+        allocate (w_temp(0:0))
       else   
       do
-       open (105,file=in_cha%temp)
-       read (105,*,iostat=eof) titldum
-       if (eof < 0) exit
-       read (105,*,iostat=eof) header
-       if (eof < 0) exit
-       read (105,*,iostat=eof) w_temp
-       if (w_temp%airlag_d < 1) w_temp%airlag_d = 6
-       !if (w_temp%hex_coef1 < 1) w_temp%hex_coef1 = 6
-       if (eof < 0) exit
-         
-        !do while (eof == 0)
-        !  read (105,*,iostat=eof) titldum
-        !  if (eof < 0) exit
-        !  imax = imax + 1
-        !end do
+        open (105,file=in_cha%temp)
+        read (105,*,iostat=eof) titldum
+        if (eof < 0) exit
+        read (105,*,iostat=eof) header
+        !if (eof < 0) exit
+        !read (105,*,iostat=eof) wa_temp
+        !if (wa_temp%airlag_d < 1) wa_temp%airlag_d = 6
+        !if (wa_temp%hex_coef1 < 1) wa_temp%hex_coef1 = 6
         
-      db_mx%ch_temp = imax
+        if (eof < 0) exit     
+          do while (eof == 0)
+            read (105,*,iostat=eof) titldum
+            if (eof < 0) exit
+            imax = imax + 1
+          end do
+        
+        db_mx%w_temp = imax
+        
+        if (allocated(w_temp)) deallocate(w_temp)
       
-      !allocate (ch_temp(0:imax))
-      !rewind (105)
-      !read (105,*,iostat=eof) titldum
-      !if (eof < 0) exit
-      !read (105,*,iostat=eof) header
-      !if (eof < 0) exit
+        allocate (w_temp(0:imax))
+        rewind (105)
+        read (105,*,iostat=eof) titldum
+        if (eof < 0) exit
+        read (105,*,iostat=eof) header
+        if (eof < 0) exit
       
-       !do ich = 1, db_mx%ch_temp
-         !read (105,*,iostat=eof) titldum
-         !if (eof < 0) exit
-         !backspace (105)
-         !read (105,*,iostat=eof) w_temp
-         !if (eof < 0) exit
-       !end do
-       close (105)
-      exit
+        do ich = 1, db_mx%w_temp
+            read (105,*,iostat=eof) titldum
+            if (eof < 0) exit
+            backspace (105)
+            read (105,*,iostat=eof) w_temp
+            if (eof < 0) exit
+        end do
+
+        exit
       enddo
       endif
-
+      close (105)
       return    
-      end subroutine ch_read_temp
+	end subroutine ch_read_temp
+	
