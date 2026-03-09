9e5c9450b4152557bd8abeef73a498c822eab809
fgeter <fgeter@gmail.com>
2026-03-09 10:20:55 -0600

    Clarified a comment in time_control regarding biomixing when cswat does not equal 2 or 3.

--- src/time_control.f90
+++ src/time_control.f90
@@ -325,7 +325,7 @@
           end if
         
           !! compute biological mixing at the end of every year
-          if (bsn_cc%cswat /= 2 .and. bsn_cc%cswat /= 3) then  !! fg added this because so that cbn_zhang2 can handle bio mixing directlyn
+          if (bsn_cc%cswat /= 2 .and. bsn_cc%cswat /= 3) then  !! fg added this because when cswat == 2 or cswat == 3, biomixing occurs every day that the soil layer is above freezing not at the end of the year.
             if (hru(j)%hyd%biomix > 1.e-6) call mgt_newtillmix (j, hru(j)%hyd%biomix, 0)
           end if
 
