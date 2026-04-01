9f1253e1291b7dcf8495af73864c086ef277d091
Celray James CHAWANDA <celray.chawanda@outlook.com>
2026-03-31 19:31:05 -0500

    gwflow re-merge: basic source/sink processes - rech, gwet, channel_exch, satexcess, tile, soil
    Migrated six process files from the gwflow implementation with full
    physics including heat transport, solute transport, monthly accumulation,
    and salt/constituent tracking. Added channel depth zone support to
    channel exchange, heat transfer to all processes, and monthly sum
    arrays throughout.

--- src/gwflow_rech.f90
+++ src/gwflow_rech.f90
@@ -3,12 +3,13 @@
 !!    ~ ~ ~ PURPOSE ~ ~ ~
 !!    this subroutine determines the volume of groundwater that is added to the aquifer via recharge (soil percolation)
 !!    (recharge volumes are used in gwflow_simulate, in groundwater balance equations)
-      
-      use gwflow_module   
+
+      use gwflow_module
       use hydrograph_module, only : ob,sp_ob,sp_ob1
       use maximum_data_module, only : db_mx
       use calibration_data_module, only : lsu_out
-      
+      use soil_module, only : soil
+
       implicit none
 
       integer :: i = 0                !           |counter
@@ -26,43 +27,54 @@
       real :: rech_volume = 0.        !m3         |summation of recharge from multiple HRUs
       real :: cell_rech_volume = 0.   !m3         |volume of recharge to the cell
       real :: rech_solmass(100) = 0.  !g          |summation of solute mass in recharge from multiple HRUs
-      real :: cell_rech_solmass(100) = 0.!g          |solute mass in recharge to the cell
+      real :: cell_rech_solmass(100) = 0. !g      |solute mass in recharge to the cell
+      real :: cell_rech_heat = 0.     !J          |heat in recharge to the cell
       real :: hru_total = 0.          !m3         |summation of recharge from multiple HRUs
       real :: hru_cell_total = 0.     !m3         |summation of recharge for multiple cells
       real :: huc12_cell_total = 0.   !m3         |summation of recharge from a huc12 catchment
       real :: sub_recharge = 0.       !m3         |summation of recharge for subbasin
+      real :: sub_heat = 0.           !J          |summation of recharge heat for subbasin
       real :: sub_solmass(100) = 0.   !g          |total solute mass in recharge, for the subbasin
-      
+      real :: perc_volm = 0.          !m3         |volume of deep percolation
+      real :: perc_temp = 0.          !deg C      |temperature of soil water in the lowest soil layer (= deep percolation)
+      real :: perc_heat = 0.          !J          |heat of deep percolation water
+      real :: recharge_heat = 0.      !J          |heat in recharge water
+
 
       !calculate recharge and solute mass to the water table
-      !open(1357,file='gwflow_daily_hru_recharge')
       do k=1,sp_ob%hru
+        ob_num = sp_ob1%hru + k - 1
         recharge = gw_rech(k)
         gw_rech(k) = 0.
         gw_rech(k) = ((1.-gw_delay(k))*gwflow_perc(k)) + (gw_delay(k)*recharge)
         if (gw_rech(k) < 1.e-6) gw_rech(k) = 0.
-        !write(1357,*) k,gw_rech(k)
-        if (gw_solute_flag == 1) then
+        if(gw_heat_flag == 1) then
+          perc_volm = (gwflow_perc(k)/1000.) * (ob(ob_num)%area_ha * 10000.) !m * m2 = m3
+          perc_temp = soil(k)%phys(soil(k)%nly)%tmp !deg C
+          perc_heat = perc_temp * gw_rho * gw_cp * perc_volm !J
+          recharge_heat = gw_rechheat(k)
+          gw_rechheat(k) = ((1.-gw_delay(k))*perc_heat) + (gw_delay(k)*recharge_heat)
+        endif
+        if(gw_solute_flag == 1) then
           do s=1,gw_nsolute !loop through the solutes
             recharge_sol = gw_rechsol(k,s)
             gw_rechsol(k,s) = ((1.-gw_delay(k))*gwflow_percsol(k,s)) + (gw_delay(k)*recharge_sol)
           enddo
         endif
       enddo
-      !write(1357,*)
 
 
 
       !use hru recharge to calculate recharge (m3) cell values
       if (lsu_cells_link == 1) then !LSU-cell connection
-      
+
         !loop through the landscape units
         do k=1,db_mx%lsu_out
           !sum recharge (m3) for each LSU (based on collection of HRUs within the LSU)
           rech_volume = 0.
           if (gw_solute_flag == 1) then
             rech_solmass = 0.
-          endif
+					endif
           do j=1,lsu_out(k)%num_tot
             hru_id = lsu_out(k)%num(j)
             ob_num = sp_ob1%hru + hru_id - 1
@@ -72,30 +84,36 @@
               do s=1,gw_nsolute !loop through the solutes
                 rech_solmass(s) = rech_solmass(s) + (gw_rechsol(hru_id,s)*ob(ob_num)%area_ha*1000.) !g
               enddo
-            endif
+			endif
           enddo
           !map recharge from LSU to grid cells connected to the LSU
           do i=1,lsu_num_cells(k)
             cell_id = lsu_cells(k,i)
-            if(gw_state(cell_id)%stat == 2) then !if boundary cell, give recharge to nearest active cell
-              cell_id = gw_bound_near(cell_id)
-            endif
+						if(gw_state(cell_id)%stat == 2) then !if boundary cell, give recharge to nearest active cell
+						  cell_id = gw_bound_near(cell_id)
+						endif
             cell_rech_volume = rech_volume * lsu_cells_fract(k,i)
             gw_hyd_ss(cell_id)%rech = gw_hyd_ss(cell_id)%rech + cell_rech_volume
-            gw_hyd_ss_yr(cell_id)%rech = gw_hyd_ss_yr(cell_id)%rech + cell_rech_volume
+            gw_hyd_ss_yr(cell_id)%rech = gw_hyd_ss_yr(cell_id)%rech + cell_rech_volume !store for annual water
+            gw_hyd_ss_mo(cell_id)%rech = gw_hyd_ss_mo(cell_id)%rech + cell_rech_volume !store for monthly water
+            if(gw_heat_flag == 1) then
+              cell_rech_heat = gw_rechheat(k) * lsu_cells_fract(k,i)
+              gw_heat_ss(cell_id)%rech = gw_heat_ss(cell_id)%rech + cell_rech_heat
+              gw_heat_ss_yr(cell_id)%rech = gw_heat_ss_yr(cell_id)%rech + cell_rech_heat
+            endif
             if(gw_solute_flag == 1) then
               do s=1,gw_nsolute !loop through the solutes
-                cell_rech_solmass(s) = rech_solmass(s) * lsu_cells_fract(k,i)  
+                cell_rech_solmass(s) = rech_solmass(s) * lsu_cells_fract(k,i)
                 gwsol_ss(cell_id)%solute(s)%rech = gwsol_ss(cell_id)%solute(s)%rech + cell_rech_solmass(s)
                 gwsol_ss_sum(cell_id)%solute(s)%rech = gwsol_ss_sum(cell_id)%solute(s)%rech + cell_rech_solmass(s)
+								gwsol_ss_sum_mo(cell_id)%solute(s)%rech = gwsol_ss_sum_mo(cell_id)%solute(s)%rech + cell_rech_solmass(s)
               enddo
             endif
           enddo
         enddo !go to next LSU
-        
-      else !proceed with HRU-cell connection
-      
-      !map recharge from the HRUs to the grid cells
+
+			else !proceed with HRU-cell connection
+
       !map recharge from the HRUs to the grid cells
       !national model (HUC12) recharge mapping removed
         ob_num = sp_ob1%hru  !object number of first HRU
@@ -108,24 +126,31 @@
           endif
           do i=1,hru_num_cells(k)
             cell_id = hru_cells(k,i)
-            if(gw_state(cell_id)%stat == 2) then !if boundary cell, give recharge to nearest active cell
-             cell_id = gw_bound_near(cell_id)
-            endif
+						if(gw_state(cell_id)%stat == 2) then !if boundary cell, give recharge to nearest active cell
+						  cell_id = gw_bound_near(cell_id)
+						endif
             cell_rech_volume = rech_volume * hru_cells_fract(k,i)
             gw_hyd_ss(cell_id)%rech = gw_hyd_ss(cell_id)%rech + cell_rech_volume
-            gw_hyd_ss_yr(cell_id)%rech = gw_hyd_ss_yr(cell_id)%rech + cell_rech_volume
+            gw_hyd_ss_yr(cell_id)%rech = gw_hyd_ss_yr(cell_id)%rech + cell_rech_volume !store for annual water
+            gw_hyd_ss_mo(cell_id)%rech = gw_hyd_ss_mo(cell_id)%rech + cell_rech_volume !store for monthly water
+            if(gw_heat_flag == 1) then
+              cell_rech_heat = gw_rechheat(k) * hru_cells_fract(k,i)
+              gw_heat_ss(cell_id)%rech = gw_heat_ss(cell_id)%rech + cell_rech_heat
+              gw_heat_ss_yr(cell_id)%rech = gw_heat_ss_yr(cell_id)%rech + cell_rech_heat
+            endif
             if (gw_solute_flag == 1) then
               do s=1,gw_nsolute !loop through the solutes
                 cell_rech_solmass(s) = rech_solmass(s) * hru_cells_fract(k,i)
                 gwsol_ss(cell_id)%solute(s)%rech = gwsol_ss(cell_id)%solute(s)%rech + cell_rech_solmass(s)
                 gwsol_ss_sum(cell_id)%solute(s)%rech = gwsol_ss_sum(cell_id)%solute(s)%rech + cell_rech_solmass(s)
+								gwsol_ss_sum_mo(cell_id)%solute(s)%rech = gwsol_ss_sum_mo(cell_id)%solute(s)%rech + cell_rech_solmass(s)
               enddo
             endif
           enddo
           ob_num = ob_num + 1
         enddo
-      
+
       endif !check for LSU-cell connection
-       
+
       return
-      end subroutine gwflow_rech      
+    end subroutine gwflow_rech
--- src/gwflow_tile.f90
+++ src/gwflow_tile.f90
@@ -3,11 +3,11 @@
 !!    ~ ~ ~ PURPOSE ~ ~ ~
 !!    this subroutine calculates the water exchange volume between irrigation canals and connected grid cells
 !!    (exchange volumes are used in gwflow_simulate, in groundwater balance equations)
-      
+
       use gwflow_module
-      use hydrograph_module, only : ch_stor
+      use hydrograph_module, only : ch_stor,ch_out_d
       use constituent_mass_module
-      
+
       implicit none
 
       integer, intent (in) :: chan_id    !       |channel number
@@ -22,45 +22,66 @@
       real :: head_diff = 0.             !m      |head difference between groundwater head and tile drain
       real :: Q = 0.                     !m3/day |tile drainage outflow rate, calculated by Darcy's Law
       real :: solmass(100) = 0.          !g      |solute mass transferred from groundwater to channel, via tile
-      
-      
-      
+      real :: heat_flux = 0.             !J      |heat in tile drainage
+      real :: chan_heat = 0.             !J      |heat in channel water
+
+
       !only proceed if tile drainage is active
       if (gw_tile_flag == 1) then
-      
+
         !record starting channel volume (m3)
         chan_volume = ch_stor(chan_id)%flo
-        
+
         !loop through the cells connected to the channel
         do k=1,gw_tile_info(chan_id)%ncon
-        
+
           !cell in connection with the channel
           cell_id = gw_tile_info(chan_id)%cells(k)
-          
+
           !only proceed if the cell is active
-          if(gw_state(cell_id)%stat == 1) then 
-          
+          if(gw_state(cell_id)%stat == 1) then
+
             !get elevation of the subsurface drain (m)
-            tile_elev = gw_state(cell_id)%elev - gw_tile_depth(cell_id) 
-          
-            !only perform calculation if water table is above tile drain            
+            tile_elev = gw_state(cell_id)%elev - gw_tile_depth(cell_id)
+
+            !only perform calculation if water table is above tile drain
             if(gw_state(cell_id)%head > tile_elev) then
-              
+
               !calculate tile drainage outflow rate using Darcy's Law
               head_diff = gw_state(cell_id)%head - tile_elev
               Q = gw_tile_drain_area(cell_id) * gw_tile_K(cell_id) * head_diff !m3/day
-              
+
               !check for available groundwater in the cell - can only remove what is there
               if(Q > gw_state(cell_id)%stor) then
                 Q = gw_state(cell_id)%stor
               endif
-              gw_state(cell_id)%stor = gw_state(cell_id)%stor - Q !update available groundwater in the cell 
+              gw_state(cell_id)%stor = gw_state(cell_id)%stor - Q !update available groundwater in the cell
               gw_hyd_ss(cell_id)%tile = Q * (-1) !leaving aquifer
-              gw_hyd_ss_yr(cell_id)%tile = gw_hyd_ss_yr(cell_id)%tile + (Q*(-1)) !leaving aquifer
-              
+              gw_hyd_ss_yr(cell_id)%tile = gw_hyd_ss_yr(cell_id)%tile + (Q*(-1)) !leaving aquifer - store for annual water
+              gw_hyd_ss_mo(cell_id)%tile = gw_hyd_ss_mo(cell_id)%tile + (Q*(-1)) !leaving aquifer - store for monthly water
+
               !add water to channel
               ch_stor(chan_id)%flo = ch_stor(chan_id)%flo + Q
-              
+
+              !heat transfer from groundwater to channel
+              if(gw_heat_flag == 1) then
+                heat_flux = gwheat_state(cell_id)%temp * gw_rho * gw_cp * Q !J
+                if(heat_flux >= gwheat_state(cell_id)%stor) then
+                  heat_flux = gwheat_state(cell_id)%stor
+                endif
+                gw_heat_ss(cell_id)%tile = heat_flux * (-1) !leaving aquifer
+                gw_heat_ss_yr(cell_id)%tile = gw_heat_ss_yr(cell_id)%tile + (heat_flux*(-1)) !leaving aquifer
+                !update temperature in channel
+                chan_heat = ch_stor(chan_id)%temp * gw_rho * gw_cp * chan_volume !J in channel
+                chan_heat = chan_heat + heat_flux
+                if(ch_stor(chan_id)%flo > 0) then
+                  ch_stor(chan_id)%temp = chan_heat / (gw_rho * gw_cp * ch_stor(chan_id)%flo)
+                else
+                  ch_stor(chan_id)%temp = 0.
+								endif
+								ch_out_d(chan_id)%temp = ch_stor(chan_id)%temp
+              endif
+
               !transfer solute mass from groundwater to channel
               if (gw_solute_flag == 1) then
                 do s=1,gw_nsolute !loop through the solutes
@@ -70,7 +91,8 @@
                   endif
                   gwsol_ss(cell_id)%solute(s)%tile = solmass(s) * (-1) !leaving aquifer
                   gwsol_ss_sum(cell_id)%solute(s)%tile = gwsol_ss_sum(cell_id)%solute(s)%tile + (solmass(s)*(-1)) !leaving aquifer
-                enddo  
+									gwsol_ss_sum_mo(cell_id)%solute(s)%tile = gwsol_ss_sum_mo(cell_id)%solute(s)%tile + (solmass(s)*(-1)) !leaving aquifer
+                enddo
                 !add solute to channel
                 ch_stor(chan_id)%no3 = ch_stor(chan_id)%no3 + (solmass(1)/1000.) !kg
                 ch_stor(chan_id)%solp = ch_stor(chan_id)%solp + (solmass(2)/1000.) !kg
@@ -79,25 +101,25 @@
                 if (gwsol_salt == 1) then
                   do isalt=1,cs_db%num_salts
                     sol_index = sol_index + 1
-                    ch_water(chan_id)%salt(isalt) = ch_water(chan_id)%salt(isalt) + (solmass(sol_index)/1000.) !kg   
+                    ch_water(chan_id)%salt(isalt) = ch_water(chan_id)%salt(isalt) + (solmass(sol_index)/1000.) !kg
                   enddo
                 endif
                 !constituents
                 if (gwsol_cons == 1) then
                   do ics=1,cs_db%num_cs
                     sol_index = sol_index + 1
-                    ch_water(chan_id)%cs(ics) = ch_water(chan_id)%cs(ics) + (solmass(sol_index)/1000.) !kg  
+                    ch_water(chan_id)%cs(ics) = ch_water(chan_id)%cs(ics) + (solmass(sol_index)/1000.) !kg
                   enddo
                 endif
               endif !end solutes
-              
+
             endif !check if groundwater head above tile drain
-            
+
           endif !check if cell is active
-      
+
         enddo !go to next tile cell
-      
+
       endif !check if tile drainage is active
-      
+
       return
-      end subroutine gwflow_tile      
+    end subroutine gwflow_tile
