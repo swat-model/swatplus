76c3f20006e02403b9d6ae00ba69b2db8e7d195d
fgeter <fgeter@gmail.com>
2026-05-06 18:27:54 -0600

    Added bmix to carbvar output.

--- src/soil_carbvar_write.f90
+++ src/soil_carbvar_write.f90
@@ -47,7 +47,7 @@
             iob = sp_ob1%hru + j - 1
             do k = 1, soil(j)%nly
                 write (4574,*) freq_label, k, int(soil(j)%phys(k)%d), time%day, time%mo, time%day_mo, time%yrc, j, ob(iob)%gis_id, ob(iob)%name, &
-                                soil1(j)%org_con_lr(k)%sut, soil(j)%ly(k)%tillagef, soil(j)%ly(k)%tillagef_biomix, soil(j)%ly(k)%tillagef_tillmix, &
+                                soil1(j)%org_con_lr(k)%sut, soil(j)%ly(k)%tillagef, soil(j)%ly(k)%bmix, soil(j)%ly(k)%tillagef_biomix, soil(j)%ly(k)%tillagef_tillmix, &
                                 soil1(j)%org_con_lr(k)%till_eff, soil1(j)%org_con_lr(k)%cdg, soil1(j)%org_con_lr(k)%ox, soil1(j)%org_con_lr(k)%cs, &
                                 soil1(j)%org_con_lr(k)%no3, soil1(j)%org_con_lr(k)%nh4, soil1(j)%org_con_lr(k)%resp, soil(j)%phys(k)%tmp, soil1(j)%emix(k) 
                 if (pco%csvout == "y") then
